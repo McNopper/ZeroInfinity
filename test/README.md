11a9bcf183df9946a0b3fdbc923d959c341636bb
Norbert Nopper <norbert@nopper.tv>
2024-05-23 18:39:30 +0200

    Switched to Google Test

--- test/README.md
+++ test/README.md
@@ -1,6 +1,7 @@
 ## How to build?
 
-1. Install [MSYS2](https://www.msys2.org/)  
+1. Install [MSYS2](https://www.msys2.org/) using Windows  
 	- Open MSYS2 console and install GCC `pacman -S mingw-w64-ucrt-x86_64-gcc`  
+	- Install Google Testing and Mocking Framework `pacman -S mingw-w64-x86_64-gtest`
 2. Install Eclipse [Eclipse IDE for C/C++ Developers](https://www.eclipse.org/downloads/packages/)  
 	- Start Eclipse and import the `test` project  
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -5,30 +5,91 @@
 
 #include "IntervalArithmetic.hpp"
 
-int main(int argc, const char* argv[])
+// Constants for convenience
+
+static constexpr auto INF = std::numeric_limits<double>::infinity();
+static constexpr auto QUIET_NAN = std::numeric_limits<double>::quiet_NaN();
+
+// Basic tests for the C++ implementation
+
+TEST(Basics, ZeroMultiplyInfinity)
 {
-    // Using infinty as a constant
-    constexpr auto inf = std::numeric_limits<double>::infinity();
+    double result = 0.0 * INF;
 
-    // Checking the lower interval bound
-    auto a = 0.0 * 0.0;
-    printf("0.0 * 0.0 = %lf\n", a);
+    EXPECT_EQ(true, std::isnan(result));
+}
 
-    // Checking the upper interval bound
-    auto b = 1.0 * inf;
-    printf("1.0 * inf = %lf\n", b);
+TEST(Basics, OnePlusInfinity)
+{
+    double result = 1.0 + INF;
 
+    EXPECT_EQ(true, std::isinf(result));
+}
+
+TEST(Basics, InfinityMinusInfinity)
+{
+    double result = INF - INF;
+
+    EXPECT_EQ(true, std::isnan(result));
+}
+
+// Tests for IntervalArithmetic implementation
+
+TEST(IntervalArithmetic, ConstructorStandard)
+{
+    IntervalArithmetic result{1.0, 10.0};
+
+    EXPECT_EQ(result.lowerBound(), 1.0);
+    EXPECT_EQ(result.upperBound(), 10.0);
+}
+
+TEST(IntervalArithmetic, ConstructorSwizzle)
+{
+    IntervalArithmetic result{10.0, 1.0};
+
+    EXPECT_EQ(result.lowerBound(), 1.0);
+    EXPECT_EQ(result.upperBound(), 10.0);
+}
+
+TEST(IntervalArithmetic, ConstructorZeroInfinity)
+{
     // Creating the [0, ∞] interval
-    auto c = IntervalArithmetic(0.0, inf);
-    printf("[%lf, %lf]\n", c.lowerBound(), c.upperBound());
+    IntervalArithmetic result{0.0, INF};
 
-    // Checking the multiplication
-    auto d = -1.0 * c;
-    printf("[%lf, %lf]\n", d.lowerBound(), d.upperBound());
+    EXPECT_EQ(result.lowerBound(), 0.0);
+    EXPECT_EQ(result.upperBound(), INF);
+}
 
-    // As expected, produces not a number
-    auto e = inf - inf;
-    printf("inf - inf = %lf\n", e);
+TEST(IntervalArithmetic, Multiplication01)
+{
+    IntervalArithmetic temp{0.0, INF};
 
-    return 0;
+    auto result = -1.0 * temp;
+
+    EXPECT_EQ(result.lowerBound(), -INF);
+    EXPECT_EQ(result.upperBound(), 0.0);
+}
+
+TEST(IntervalArithmetic, Multiplication02)
+{
+    IntervalArithmetic temp{-INF, 0.0};
+
+    auto result = -1.0 * temp;
+
+    EXPECT_EQ(result.lowerBound(), 0.0);
+    EXPECT_EQ(result.upperBound(), INF);
+}
+
+TEST(IntervalArithmetic, BoundsNaN)
+{
+    IntervalArithmetic result{0.0, QUIET_NAN};
+
+    EXPECT_EQ(true, std::isnan(result.lowerBound()));
+    EXPECT_EQ(true, std::isnan(result.upperBound()));
+}
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
